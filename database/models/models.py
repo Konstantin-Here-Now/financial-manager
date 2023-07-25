456ed657ce00715f9ea6b7a557921eabeb459eb0
Konstantin-Here-Now <tyujas@gmail.com>
2023-07-26 00:28:40 +0300

    Created basic models, recreated initial migration because of problems

--- database/models/models.py
+++ database/models/models.py
@@ -2,24 +2,55 @@ from datetime import datetime
 
 from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, ForeignKey
-from sqlalchemy import Integer, String, Numeric, UUID, TIMESTAMP
+from sqlalchemy import Integer, String, Numeric, DateTime
 
 metadata = MetaData()
 
 user = Table(
     "user",
     metadata,
-    Column("id", UUID, primary_key=True),
-    Column("email", String, nullable=False, unique=True),
-    Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("id", String, primary_key=True),
+    Column("email", String(60), nullable=False, unique=True),
+    Column("username", String(60), nullable=False),
+    Column("password", String(60), nullable=False),
+    Column("registered_at", DateTime, default=datetime.utcnow),
 )
 
 account = Table(
     "account",
     metadata,
-    Column("id", UUID, primary_key=True),
+    Column("id", String, primary_key=True),
     Column("money", Numeric(12, 2)),
-    Column("user_id", UUID, ForeignKey(user.c.id), unique=True),
+    Column("user_id", String, ForeignKey(user.c.id), unique=True),
+)
+
+
+category = Table(
+    "category",
+    metadata,
+    Column("id", String, primary_key=True),
+    Column("name", String(60), nullable=False),
+    Column("user_id", String, ForeignKey(user.c.id)),
+)
+
+transaction = Table(
+    "transaction",
+    metadata,
+    Column("id", String, primary_key=True),
+    Column("operation", String(30), nullable=False),
+    Column("amount", Numeric(12, 2)),
+    Column("date", DateTime, default=datetime.utcnow),
+    Column("user_id", String, ForeignKey(user.c.id)),
+    Column("category_id", String, ForeignKey(category.c.id))
+)
+
+regular_transaction = Table(
+    "regular_transaction",
+    metadata,
+    Column("id", String, primary_key=True),
+    Column("operation", String(20), nullable=False),
+    Column("amount", Numeric(12, 2), nullable=False),
+    Column("regularity", String(20), nullable=False),
+    Column("end_date", DateTime),
+    Column("user_id", String, ForeignKey(user.c.id)),
 )
